19b6df550410e21258e61cc0399ec10f6056b493
Matheus Rocha <01devmath@gmail.com>
2022-06-11 16:51:57 -0300

    Update folder structure and add ex4

---
+++ cod3r/web-moderno/exercicios-javascript/ex4.js
@@ -0,0 +1,4 @@
+const nomeDoMes = (num) => {
+  const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+  return meses[--num];
+}
