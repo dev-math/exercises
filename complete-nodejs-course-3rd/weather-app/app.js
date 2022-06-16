e05a7b7adf851f2279a3dfae0143bec7b5dc6cc0
Matheus Rocha <01devmath@gmail.com>
2022-06-16 10:23:13 -0300

    Add error handling

--- complete-nodejs-course-3rd/weather-app/app.js
+++ complete-nodejs-course-3rd/weather-app/app.js
@@ -1,26 +1,31 @@
 const request = require('postman-request');
+
 require('dotenv').config();
+const location = "Sao Paulo";
+const IP_API = process.env.IP_STACK;
+const WEATHER_API = process.env.WEATHER_STACK;
 
-const location = "Sao Paulo"
+const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHER_API}&query=${location}`;
+request(weatherURL, {json: true}, (err, res) => {
+  if (err) {
+    console.log(`Unable to connect to weatherstack!`);
+    return;
+  } else if (res.body.error) {
+    console.log(`Location not found`);
+  } else {
+    const data = res.body.current;
+    console.log(`${data.weather_descriptions[0]}. It is current ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`);
+  }
+});
 
-// const WEATHER_API = process.env.WEATHER_STACK;
-// const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHER_API}&query=${location}`
-// request(weatherURL, {json: true}, (_err, res) => {
-//   const data = res.body.current;
-//   console.log(`${data.weather_descriptions[0]}. It is current ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`);
-// });
-
-// const POSITION_API = process.env.POSITION_STACK;
-// const positionURL = `http://api.positionstack.com/v1/forward?access_key=${POSITION_API}&query=${location}&limit=1`
-// request(positionURL, {json: true}, (_err, res) => {
-//   const data = res.body.data[0];
-//   console.log(`${data.latitude} ${data.longitude}`);
-// });
-
-
-// const IP_API = process.env.IP_STACK;
-// const ipURL = `http://api.ipstack.com/check?access_key=${IP_API}`
-// request(ipURL, {json: true}, (_err, res) => {
-//   const data = res.body;
-//   console.log(`${data.latitude} ${data.longitude}`);
-// });
+const ipURL = `http://api.ipstack.com/check?access_key=${IP_API}`;
+request(ipURL, {json: true}, (err, res) => {
+  if (err) {
+    console.log(`Unable to connect to ipstack!`);
+  } else if (res.body.error) {
+    console.log(res.body.error.info);
+  } else {
+    const data = res.body;
+    console.log(`${data.latitude} ${data.longitude}`);
+  }
+});
